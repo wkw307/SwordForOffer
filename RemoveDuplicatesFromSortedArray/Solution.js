8c08ffec4bd7bec8494ff594600bc433dbb70ea0
wkw307 <wkw307@gmail.com>
2019-08-04 13:54:44 +0800

    Remove Duplicates from Sorted Array

---
+++ RemoveDuplicatesFromSortedArray/Solution.js
@@ -0,0 +1,22 @@
+// two pointers
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  if (nums.length === 0 || nums.length === 1) return nums;
+  let left = 0;
+  let right = 1;
+  while (right < nums.length) {
+    if (nums[left] === nums[right]) {
+      right++;
+    } else {
+      left++;
+      nums[left] = nums[right];
+      right++;
+    }
+  }
+  return left + 1;
+};
+
+console.log(removeDuplicates([1,1,2]));
